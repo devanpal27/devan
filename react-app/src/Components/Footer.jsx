3180a5b978bf8b8aff954da5053e7be213ae3258
Devan Pal <paldevan2712@gmail.com>
2023-10-19 09:41:14 +0530

    Third Commit

--- react-app/src/Components/Footer.jsx
+++ react-app/src/Components/Footer.jsx
@@ -9,8 +9,10 @@ class Footer extends Component{
             <hr />
             <div id="bo" className="text-center">
              <h3 id="h">Contact us</h3>
+             <div id="col">
              <b>Mob:</b><p>987654321</p>
              <b>Email:</b><p>abc@hmail.com</p>
+             </div>
             </div>
             </>
         )
